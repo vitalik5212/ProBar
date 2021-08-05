2e21ab0d2097e7513cfa415a620f53d736a9221b
Vitalij <r.vitalik.skuratov@gmail.com>
2021-08-05 22:19:04 +0300

    Modified html views. Create registration form. Added link for registration form, login and logout. Added this function in controllers.

--- src/main/java/com/lessons/home/springsecurity/controllers/MainController.java
+++ src/main/java/com/lessons/home/springsecurity/controllers/MainController.java
@@ -1,17 +1,26 @@
 package com.lessons.home.springsecurity.controllers;
 
 import com.lessons.home.springsecurity.services.GeneralService;
+import com.lessons.home.springsecurity.entity.user.User;
+import com.lessons.home.springsecurity.services.RoleService;
+import com.lessons.home.springsecurity.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
     private final GeneralService generalService;
+    private final UserService userService;
+    private final RoleService roleService;
 
-    public MainController(GeneralService generalService) {
+    public MainController(GeneralService generalService, UserService userService, RoleService roleService) {
         this.generalService = generalService;
+        this.userService = userService;
+        this.roleService = roleService;
     }
 
     @GetMapping("/")
@@ -23,8 +32,21 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("login")
-    public String login() {
-        return "login";
+    @GetMapping("/registration")
+    public String login(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "user/registration";
+    }
+
+    @PostMapping("logout")
+    public String logout() {
+        return "redirect:/";
+    }
+
+    @PostMapping("/registration")
+    public String newUser(@ModelAttribute User user) {
+            userService.create(user, roleService.getRoleByName("USER"));
+        return "redirect:/";
     }
 }
