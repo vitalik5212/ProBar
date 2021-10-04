2004c2714caac66f782b9c490b97b60ec96fa890
Vitalij <r.vitalik.skuratov@gmail.com>
2021-10-04 16:28:32 +0300

    Fixed the UserService. Changed Security config and added CorsConfiguration.

--- src/main/java/com/lessons/home/springsecurity/services/UserService.java
+++ src/main/java/com/lessons/home/springsecurity/services/UserService.java
@@ -19,8 +19,7 @@ public class UserService implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) {
        User user = userRepository.getUserByUsername(username);
 
        return new MyUserDetails(user);
