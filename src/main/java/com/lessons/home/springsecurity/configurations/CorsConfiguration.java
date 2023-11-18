b6d3f92198d642381ede95ba2c0dbbd89fbff017
Modificks <dimanakonechnui7@gmail.com>
2023-11-19 01:08:38 +0200

    test pull request

--- src/main/java/com/lessons/home/springsecurity/configurations/CorsConfiguration.java
+++ src/main/java/com/lessons/home/springsecurity/configurations/CorsConfiguration.java
@@ -2,6 +2,7 @@ package com.lessons.home.springsecurity.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
